8cb56fbb6e497fdfbe422665298e08fae794a38a
Vince Dev <vincelp@hotmail.com>
2025-08-13 21:02:24 -0400

    Add login and register links to header menu for unauthenticated users

--- McWuT/Program.cs
+++ McWuT/Program.cs
@@ -16,7 +16,7 @@ namespace McWuT.Web
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+         
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                             .AddRoles<IdentityRole>()
